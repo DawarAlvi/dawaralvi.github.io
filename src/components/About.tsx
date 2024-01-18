600883f62ef530f9609934328573c0f0efc20124
Syed Dawar Alvi <46403138+DawarAlvi@users.noreply.github.com>
2024-01-19 01:20:31 +0530

    about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,27 +1,30 @@
+import aboutImg from "../assets/img/about-img.png";
+
 export const About = () => {
   return (
     <section className="about">
       <div className="wrapper">
         <div className="shrink-wrapper">
-          <img
-            alt="crypto"
-            src="https://source.unsplash.com/random/?computer&1"
-          />
+          <img alt="crypto" src={aboutImg} />
           <div>
-            <span className="about__about-me">ABOUT ME</span>
+            <h3 className="section-title">ABOUT ME</h3>
             <h2 className="about__title">
-              Front-end Developer with a knack for design
+              Front-end Developer with a knack for clean design
             </h2>
 
             <p className="about__content">
-              I love learning about technology and coming up with creative
-              solutions to difficult problems.
+              As a front-end developer I excel at creating seamless and
+              intuitive and user experiences.
             </p>
 
             <p className="about__content">
-              My hobbies include hiking, reading, art and gaming. Story driven
-              games that inspire and appeal to one's innate curiosity like Outer
-              Wilds, Journey and The Witness are my favourite.
+              I love learning about technology and coming up with creative
+              solutions to challenging problems.
+            </p>
+
+            <p className="about__content">
+              Other than programming, I also love hiking, reading, creating art
+              and retro gaming.
             </p>
           </div>
         </div>
