7d231c395b522b0afdf849e95e543c89a41d67a0
Syed Dawar Alvi <46403138+DawarAlvi@users.noreply.github.com>
2024-01-19 19:13:36 +0530

    projects layout

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,15 +4,52 @@ export const Projects = () => {
   return (
     <section className="projects" id="projects">
       <div className="wrapper">
-        <h3 className="section-title">PROJECTS</h3>
+        <h3 className="section-title">FEATURED PROJECTS</h3>
         <div className="project-list">
           <div className="project">
-            <img
-              src="https://source.unsplash.com/random/?website&size=400x300"
-              alt=""
-            />
-            <div>
-              <h4>Encode Academy</h4>
+            <img src="./assets/img/encode-academy.png" alt="" />
+            <div className="project__info">
+              <h4 className="project__name">Encode Academy</h4>
+              <p>
+                Encode Academy is an online learning platform where the users
+                can enroll in courses and learn from a variety of topics.
+              </p>
+              <div className="tags">
+                <span>React</span>
+                <span>JavaScript</span>
+              </div>
+              <div className="links">
+                <a href="https://encodeacademy.dev" target="_blank">
+                  Live Demo <RxExternalLink />
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="project">
+            <img src="./assets/img/encode-academy.png" alt="" />
+            <div className="project__info">
+              <h4 className="project__name">Encode Academy</h4>
+              <p>
+                Encode Academy is an online learning platform where the users
+                can enroll in courses and learn from a variety of topics.
+              </p>
+              <div className="tags">
+                <span>React</span>
+                <span>JavaScript</span>
+              </div>
+              <div className="links">
+                <a href="https://encodeacademy.dev" target="_blank">
+                  Live Demo <RxExternalLink />
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="project">
+            <img src="./assets/img/encode-academy.png" alt="" />
+            <div className="project__info">
+              <h4 className="project__name">Encode Academy</h4>
               <p>
                 Encode Academy is an online learning platform where the users
                 can enroll in courses and learn from a variety of topics.
